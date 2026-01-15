bf8f0fddf982d5755f8f2776e586fa08310a9d5d
Thiago <thiagocascaes@outlook.com,br>
2026-01-15 17:38:55 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -11,9 +11,9 @@ describe('Orange HRM tests', () => {
     wrongCredentialAlert: '.oxd-alert-content > .oxd-text'
   }
 
-  
+
   it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
@@ -21,7 +21,7 @@ describe('Orange HRM tests', () => {
     cy.get(selectorsList.dashboardGrid)
   })
     it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
